1e0b78245757e7d8b35bff00aaa3ea6d003284d2
chaukhaluan462-droid <chaukhaluan462@gmail.com>
2026-08-09 13:22:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -224,4 +224,4 @@ client.on('interactionCreate', async interaction => {
 
 // Điền Token Bot của bạn vào đây 👇
 client.login('YOUR_BOT_TOKEN');
-client.login(process.env.DISCORD_TOKEN);
+client.login('đoạn_token_thật_của_bạn_ở_đây');
